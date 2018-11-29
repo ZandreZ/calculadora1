f2e5b006f967cd2e7a51468edf77999d5ba3dd5d
ZandreZ <43865780+ZandreZ@users.noreply.github.com>
2018-11-29 20:03:15 -0200

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,21 @@
+function botao (num)
+{
+    var salvo = document.calculadora.visor.value;
+    document.calculadora.visor.value = salvo + num;
+}
+
+function reset () 
+{
+ document.getElementById('visor').value = '';
+}   
+
+function calcular () 
+{
+  var resultado = 0;
+  resultado = document.getElementById('visor').value;
+  
+  document.getElementById('visor').value = '';
+  document.getElementById('visor').value = eval(resultado);
+  // alert(resultado); funcionando
+
+}
